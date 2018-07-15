aab1da06209270ef4d3e884f4a98ff99baf81c0f
funador <jesse.heaslip@gmail.com>
2018-07-15 16:48:09 -0500

    adding socketId

--- src/OAuth.js
+++ src/OAuth.js
@@ -15,9 +15,6 @@ class OAuth extends Component {
 
   componentDidMount() {
     const { socket, provider } = this.props
-    socket.on('connect', () => {
-      socket.emit('auth', socket.id)  
-    })
 
     socket.on(provider, user => {  
       this.popup.close()
@@ -36,11 +33,11 @@ class OAuth extends Component {
   }
 
   openPopup() {
-    const { provider } = this.props
+    const { provider, socket } = this.props
     const width = 600, height = 600
     const left = (window.innerWidth / 2) - (width / 2)
     const top = (window.innerHeight / 2) - (height / 2)
-    const url = `${API_URL}/${provider}`
+    const url = `${API_URL}/${provider}?socketId=${socket.id}`
 
     return window.open(url, '',       
       `toolbar=no, location=no, directories=no, status=no, menubar=no, 
