4a739a416d7351444f3d328ed44bc4c66617d845
funador <jesse.heaslip@gmail.com>
2018-07-21 11:01:10 -0500

    adding medium to footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,18 @@ const Footer = () => {
   return (
     <footer>
       <a 
+        href='https://medium.com/p/2f6b7b0ee9d2/' 
+        title='Medium Article'
+        className={'small-button medium'}
+      >
+        <FontAwesome
+          name={'medium'}
+        />
+      </a>
+      <a 
         href='https://github.com/funador/react-auth-client' 
         title='Github repo'
-        className={'small-button'}
+        className={'small-button github'}
       >
         <FontAwesome
           name={'github'}
