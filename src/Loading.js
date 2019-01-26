bd4f59ad78494367955be85208cc5e12a529a4ce
funador <jesse.heaslip@gmail.com>
2019-01-25 23:08:07 -0600

    removing syntax from Loading.js

--- src/Loading.js
+++ src/Loading.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 export default () =>
-  <div className={'loading-wrapper fadein-slow'}>
+  <div className='loading-wrapper fadein-slow'>
     <h4>Heroku is spinning up, one moment please...</h4>
-    <div className={'loading'}>
-      <div className={'background'}>
+    <div className='loading'>
+      <div className='background'>
         <i className="icon-heroku"></i>
       </div>
-      <div className={'spinner'} />
+      <div className='spinner' />
     </div>
   </div>
